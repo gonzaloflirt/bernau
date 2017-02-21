44639e14a660ca616dc51a9aceb18bc9e6ddf6d8
gonzalo <gonzalo.flirt@gmail.com>
2017-02-21 19:29:28 +0100

    Add OSC receiver

--- src/server/ControllerExperience.js
+++ src/server/ControllerExperience.js
@@ -8,22 +8,30 @@ export default class ControllerExperience extends Experience {
     this.checkin = this.require('checkin');
     this.sharedConfig = this.require('shared-config');
     this.sync = this.require('sync');
+    this.osc = this.require('osc');
   }
 
   start() {
-    // ...
+    this.osc.receive('/play', (index) => {
+      this.broadcastPlay(index)
+    });
   }
 
   enter(client) {
     super.enter(client);
+
     this.receive(client, 'play', (index) => {
-      var syncTime = this.sync.getSyncTime() + 2;
-      this.broadcast(null, null, 'play', index, syncTime);
+      this.broadcastPlay(index)
     });
   }
 
   exit(client) {
     super.exit(client);
     // ...
+  }
+
+  broadcastPlay(index) {
+    var syncTime = this.sync.getSyncTime() + 2;
+    this.broadcast(null, null, 'play', index, syncTime);
   }
 }
