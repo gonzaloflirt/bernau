0298a0fd65c88dc778d35d2244f365997d3d3ef6
gonzalo <gonzalo.flirt@gmail.com>
2017-04-03 22:25:29 +0200

    Add scheduler to controller to iterate through scenes

--- src/client/controller/ControllerExperience.js
+++ src/client/controller/ControllerExperience.js
@@ -1,7 +1,43 @@
 import * as soundworks from 'soundworks/client';
 
+var scene = -1;
+
 export default class ControllerExperience extends soundworks.BasicSharedController {
-  constructor(options) {
+
+  constructor(files, options) {
     super(options);
+
+    this.files = files;
+    this.params = this.require('shared-params');
+    this.scheduler = this.require('scheduler');
+    this.sync = this.require('sync');
+    this.loader = this.require('loader', {
+      files: files,
+    });
+  }
+
+  start() {
+    super.start();
+    this.params.addParamListener('playing', (value) => this.playingChanged(value));
+  }
+
+  playingChanged(value) {
+    if (value) {
+      scene = -1;
+      this.iterateScene();
+    }
+  }
+
+  iterateScene() {
+    if (this.sharedParams.params['playing'].value)
+    {
+      scene = (scene + 1) % this.files.length;
+      var syncTime = this.sync.getSyncTime() + 1;
+      this.params.params['scene'].update(scene.toString() + ' ' + syncTime.toString());
+      var sceneDuration = this.loader.buffers[scene].duration;
+      this.scheduler.defer(function(){
+        this.iterateScene() }.bind(this),
+        this.sync.getSyncTime() + sceneDuration);
+    }
   }
 }
--- src/client/controller/index.js
+++ src/client/controller/index.js
@@ -2,6 +2,11 @@ import * as soundworks from 'soundworks/client';
 import ControllerExperience from './ControllerExperience';
 import viewTemplates from '../shared/viewTemplates';
 import viewContent from '../shared/viewContent';
+import audioFileNames from '../shared/audioFileNames';
+
+const files = audioFileNames.map(function(name) {
+  return 'sounds/' + name + '.mp3'
+});
 
 window.addEventListener('load', () => {
   const { appName, clientType, socketIO, assetsDomain }  = window.soundworksConfig;
@@ -10,7 +15,7 @@ window.addEventListener('load', () => {
   soundworks.client.setViewContentDefinitions(viewContent);
   soundworks.client.setViewTemplateDefinitions(viewTemplates);
 
-  const experience = new ControllerExperience();
+  const experience = new ControllerExperience(files);
 
   soundworks.client.start();
 });
