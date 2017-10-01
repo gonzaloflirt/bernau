073c81ed6804195226bb1a026c7bf8126b065262
gonzalo flirt <gonzalo.flirt@gmail.com>
2017-10-01 21:52:22 +0200

    Handle parameter changes on server

--- src/client/controller/ControllerExperience.js
+++ src/client/controller/ControllerExperience.js
@@ -1,51 +1,11 @@
 import * as soundworks from 'soundworks/client';
-import score from '../shared/score'
-import util from '../shared/util';
 
 export default class ControllerExperience extends soundworks.ControllerExperience {
 
   constructor(assetsDomain) {
     super();
-    this.platform = this.require('platform', { features: ['web-audio'] });
-    this.checkin = this.require('checkin', { showDialog: false });
-    this.sync = this.require('sync');
     this.params = this.require('shared-params');
     this.auth = this.require('auth');
-  }
-
-  start() {
-    super.start();
-    this.firstCallback = true;
-    this.params.addParamListener('playing', (value) => this.playingChanged(value));
-    this.params.addParamListener('next', () => this.nextTriggered());
-    this.params.addParamListener('prev', () => this.prevTriggered());
-  }
-
-  playingChanged(value) {
-    if (this.firstCallback)
-    {
-      this.firstCallback = false;
-      return;
-    }
-
-    const currentTime = this.sync.getSyncTime() + 1;
-    var state = util.decodeState(this.params.getValue('state'));
-    this.params.update('state', util.encodeState(value, state.index, currentTime));
-  }
-
-  nextTriggered() {
-    const currentTime = this.sync.getSyncTime() + 1;
-    var state = util.decodeState(this.params.getValue('state'));
-    var index = util.increaseStateIndex(state.index);
-    this.params.update('state', util.encodeState(state.playing, index, currentTime));
-  }
-
-  prevTriggered() {
-    const currentTime = this.sync.getSyncTime() + 1;
-    var state = util.decodeState(this.params.getValue('state'));
-    var index = util.decreaseStateIndex(state.index);
-    this.params.update('state',
-      util.encodeState(state.playing, index, currentTime));
   }
 
 }
--- src/server/ControllerExperience.js
+++ src/server/ControllerExperience.js
@@ -1,4 +1,6 @@
 import { Experience } from 'soundworks/server';
+import score from '../client/shared/score'
+import util from '../client/shared/util';
 
 export default class ControllerExperience extends Experience {
 
@@ -12,7 +14,42 @@ export default class ControllerExperience extends Experience {
     this.auth = this.require('auth');
   }
 
-  start() { }
+  start() {
+    super.start();
+    this.firstCallback = true;
+    this.params.addParamListener('playing', (value) => this.playingChanged(value));
+    this.params.addParamListener('next', () => this.nextTriggered());
+    this.params.addParamListener('prev', () => this.prevTriggered());
+  }
+
+  state() {
+    return util.decodeState(this.params.params.state.data.value);
+  }
+
+  playingChanged(value) {
+    if (this.firstCallback)
+    {
+      this.firstCallback = false;
+      return;
+    }
+
+    const currentTime = this.sync.getSyncTime() + 1;
+    this.params.update('state', util.encodeState(value, this.state().index, currentTime));
+  }
+
+  nextTriggered() {
+    const currentTime = this.sync.getSyncTime() + 1;
+    var state = this.state();
+    var index = util.increaseStateIndex(state.index);
+    this.params.update('state', util.encodeState(state.playing, index, currentTime));
+  }
+
+  prevTriggered() {
+    const currentTime = this.sync.getSyncTime() + 1;
+    var state = this.state();
+    var index = util.decreaseStateIndex(state.index);
+    this.params.update('state', util.encodeState(state.playing, index, currentTime));
+  }
 
   enter(client) {
     super.enter(client);
